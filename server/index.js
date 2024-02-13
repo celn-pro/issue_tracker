a496ba7724afe517308af40ffc708a7c2053be76
celn-pro <ayouberasto7@gmail.com>
2024-02-13 19:44:10 +0300

    updated cors

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 const app = express()
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://odd22-celn-pro.vercel.app"); 
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); 
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
